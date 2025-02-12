aca8c7c55bba926c438aebc4468527336c1e2def
Damian Kędra <dkedra@esus-it.com>
2025-02-12 19:38:55 +0100

    add loading spinner to form

--- resources/views/livewire/modals/category-names-modal.blade.php
+++ resources/views/livewire/modals/category-names-modal.blade.php
@@ -18,13 +18,16 @@
                 </div>
             @endforeach
 
-            <div class="px-5">
+            <div class="flex gap-3 px-5">
                 <x-primary-button @click="edit =! edit">{{ __('Edit') }}</x-primary-button>
                 <x-secondary-button x-show="edit" wire:click="translate">{{ __('Translate') }}</x-secondary-button>
             </div>
+            <div wire:loading class="flex justify-center items-center ml-3 gap-2">
+                <i class="fa-solid fa-spinner animate-spin"></i> {{ __('Translation...') }}
+            </div>
         </div>
         <div class="flex flex-row-reverse gap-2 pt-5">
-            <x-secondary-button x-show="edit">
+            <x-secondary-button x-show="edit" wire:click="dispatch('closeModal')">
                 {{ __('Cancel') }}
             </x-secondary-button>
             <x-primary-button wire:click="save" x-show="edit">
