4d6f74e000ca02c09a15de51eb503e7b61383fa4
Damian Kędra <dkedra@esus-it.com>
2025-02-22 14:59:33 +0100

    refactor parameter form to separate creation of parameters and values

--- app/Livewire/Forms/ParameterForm.php
+++ app/Livewire/Forms/ParameterForm.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Forms;
 
 use App\Enums\Languages;
 use App\Models\Parameter;
+use App\Models\ParameterValue;
 use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Validate;
 use Livewire\Form;
@@ -27,19 +28,47 @@ class ParameterForm extends Form
         $this->validate();
 
         try {
-            DB::transaction(function () {
-                $parameter = Parameter::query()
-                                      ->create();
-                $parameter->names()
-                          ->create($this->parameterNames);
-            });
+          $this->createParameterWithNames();
+          $this->createValuesWithNames();
         } catch (\Exception $exception) {
-
+            dd($exception->getMessage());
         }
     }
 
     public function update()
     {
 
+    }
+
+    private function createParameterWithNames(): void
+    {
+        DB::transaction(function () {
+            $parameter = Parameter::query()
+                                  ->create();
+
+            $names = [];
+            foreach ($this->parameterNames as $language => $name) {
+                $names[] = ['language' => $language, 'name' => $name];
+            }
+
+            $parameter->names()
+                      ->createMany($names);
+        });
+    }
+
+    private function createValuesWithNames(): void
+    {
+        DB::transaction(function () {
+            foreach ($this->valueNames as $valueName) {
+                $value = ParameterValue::query()->create();
+
+                $names = [];
+                foreach ($valueName as $language => $name) {
+                    $names[] = ['language' => $language, 'name' => $name];
+                }
+
+                $value->names()->createMany($names);
+            }
+        });
     }
 }
