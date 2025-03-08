50840683b208047f383df57f6e2b74cf4aa3cd30
Damian Kędra <dkedra@esus-it.com>
2025-03-08 09:53:01 +0100

    feat: add ProductFile model and establish relationship with Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,25 +8,32 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 /**
  * @property int $id
  * @property string $name
- * @property int $brand_id
  * @property string $sku
  * @property string $ean
  * @property int $price
  * @property int $quantity
+ * @property int $brand_id
+ * @property int $file_id
  */
 class Product extends Model
 {
     protected $fillable = [
         'name',
-        'brand_id',
         'sku',
         'ean',
         'price',
         'quantity',
+        'brand_id',
+        'file_id',
     ];
 
     public function brand(): HasOne
     {
         return $this->hasOne(Brand::class, 'id', 'brand_id');
+    }
+
+    public function file(): HasOne
+    {
+        return $this->hasOne(ProductFile::class, 'product_id', 'id');
     }
 }
---
+++ app/Models/ProductFile.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $path
+ * @property bool $is_processing
+ */
+class ProductFile extends Model
+{
+    protected $fillable = [
+        'name',
+        'path',
+        'is_processing',
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+}
